2ad7f0d028ec2d87c28421f224d624d9468f2b2d
= <caothechung123@gmail.com>
2020-06-12 13:08:33 +0700

    update

--- public/js/btnDropdown.js
+++ public/js/btnDropdown.js
@@ -7,10 +7,10 @@ function myFunction() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+        console.log("drop")
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
