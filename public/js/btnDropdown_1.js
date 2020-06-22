257630de0902c6df32afb8243891a5f18fde5e89
= <caothechung123@gmail.com>
2020-06-22 22:18:33 +0700

    22062020

--- public/js/btnDropdown_1.js
+++ public/js/btnDropdown_1.js
@@ -1,20 +1,19 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show1");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropbtn1')) {
         console.log("drop")
-        const dropdowns = document.getElementsByClassName("dropdown-content");
+        const dropdowns = document.getElementsByClassName("dropdown-content1");
         for (let i = 0; i < dropdowns.length; i++) {
             const openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains('show1')) {
+                openDropdown.classList.remove('show1');
             }
         }
     }
 }
-
--- public/js/btnDropdown_2.js
+++ public/js/btnDropdown_2.js
@@ -1,20 +1,19 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show2");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropbtn2')) {
         console.log("drop")
-        const dropdowns = document.getElementsByClassName("dropdown-content");
+        const dropdowns = document.getElementsByClassName("dropdown-content2");
         for (let i = 0; i < dropdowns.length; i++) {
             const openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains('show2')) {
+                openDropdown.classList.remove('show2');
             }
         }
     }
 }
-
